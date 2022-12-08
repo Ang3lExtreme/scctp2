1552bdbdcc1deca7899a59389920adf38bef9948
unknown <talm.oliveira@campus.fct.unl.pt>
2022-12-08 11:57:27 +0000

    mongo maven dependency and media resource changes

--- tp1-51162-45576/src/scc/Controllers/MediaResource.java
+++ tp1-51162-45576/src/scc/Controllers/MediaResource.java
@@ -8,6 +8,11 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 import scc.utils.Hash;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,52 +24,50 @@ import jakarta.ws.rs.core.MediaType;
  * Resource for managing media files, such as images.
  */
 @Path("/media")
-public class MediaResource
-{
-	Map<String,byte[]> map = new HashMap<String,byte[]>();
+public class MediaResource {
+	public static final String DIR = "/mnt/vol";
+
+	Map<String, byte[]> map = new HashMap<String, byte[]>();
 	String storageConnectionString = System.getenv("BlobStoreConnection");
 	BlobContainerClient containerClientImages = new BlobContainerClientBuilder()
-                .connectionString(storageConnectionString)
-                .containerName("images")
-                .buildClient();
-
+			.connectionString(storageConnectionString).containerName("images").buildClient();
 
 	BlobClient blob;
 
-
-	//upload image
+	// upload image
 	@POST
 	@Path("/")
 	@Consumes(MediaType.MEDIA_TYPE_WILDCARD)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String UploadImages(byte[] data){
-		String filename = Hash.of(data);
-		blob = containerClientImages.getBlobClient(filename);
-		//upload images with hash as name
-		blob.upload(BinaryData.fromBytes(data));
-		return filename;
+	public String UploadImages(byte[] data) throws IOException {
+		// TODO: review key generation...
+		// String key = UUID.randomUUID().toString().substring(0,8);
+		String temp = "X";
+		try {
+			File f = new File(DIR + temp);
+
+			try (FileOutputStream outputstream = new FileOutputStream(f)) {
+				outputstream.write(data);
+			}
+			return temp;
+		} catch (FileNotFoundException e) {
+			throw new FileNotFoundException(e.getMessage());
+		}
 	}
 
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_OCTET_STREAM)
-	public byte[] DownloadImages(@PathParam("id") String filename){
-		blob = containerClientImages.getBlobClient(filename);
-
-		//if blob does not exist throw error
-		if(!blob.exists()){
-			throw new WebApplicationException("Image does not exist", 404);
+	public Response DownloadImages(@PathParam("id") String filename) {
+		try {
+			File file = new File(DIR + filename);
+			if (!file.exists())
+				return Response.status(400).entity("Image does not exist.").build();
+			byte[] bytes = Files.readAllBytes(file.toPath());
+			return Response.ok(bytes).build();
+		} catch (Exception e) {
+			throw new InternalServerErrorException(e);
 		}
 
-		BinaryData data = blob.downloadContent();
-		byte[] arr = data.toBytes();
-		return arr;
-}
-
-
-
-
-
-
-
+	}
 }
